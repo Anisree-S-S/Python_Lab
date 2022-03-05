4fd158bee64247de3ce2858c6e729b2dcab900f0
Anisree-S-S <96193534+Anisree-S-S@users.noreply.github.com>
2022-03-05 17:49:43 +0530

    Add files via upload

---
+++ large.py
@@ -0,0 +1,9 @@
+a=int(input("enter the first number "))
+b=int(input("enetr the second number "))
+c=int(input("enter the second number "))
+if a>b and a>c:
+    print("the largest number is ",a)
+elif b>c:
+    print("the largest number is",b)
+else:
+    print("the largest number is ",c)
