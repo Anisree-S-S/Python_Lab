b51151eaa5c49e02f1d516cc07970cd58935a7cc
Anisree-S-S <96193534+Anisree-S-S@users.noreply.github.com>
2022-03-07 11:46:12 +0530

    S1 Exam

---
+++ string.py
@@ -0,0 +1,6 @@
+str1=(input("enter the first string :"))
+str2=(input("enter the second string :"))
+first1=str1[0]
+first2=str2[0]
+print("the strings are : ",str1," ",str2)
+print("after swapping the strings : "+first2+str1[1:]+"  "+first1+str2[1:])
